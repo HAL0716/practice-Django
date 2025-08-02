8bca874b92b36bad8a55643711c419e0542c4754
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2025-08-02 16:30:22 +0900

    Dev TopPage

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,34 +1,35 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client, RequestFactory
+
+from snippets.models import Snippet
+from snippets.views import top
+
+UserModel = get_user_model()
 
 
-class TopPageTest(TestCase):
-    def test_top_returns_200(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+class TopPageRenderSnippetsTest(TestCase):
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            username="test_user",
+            email='test@example.com',
+            password="top_secret_pass0001",
+        )
+        self.snippet = Snippet.objects.create(
+            title="title1",
+            code="print('hello')",
+            description="decription1",
+            created_by=self.user,
+        )
     
-    def test_top_returns_expected_content(self):
-        response = self.client.get("/")
-        self.assertEqual(response.content, b"Hello World")
-
-
-from django.urls import resolve
-
-from snippets.views import top, snippet_new, snippet_edit, snippet_detail
-
-
-class CreateSnippetTest(TestCase):
-    def test_should_resolve_snippet_new(self):
-        found = resolve("/snippets/new/")
-        self.assertEqual(snippet_new, found.func)
-
-
-class SnippetDetailTest(TestCase):
-    def test_should_resolve_snippet_detail(self):
-        found = resolve("/snippets/1/")
-        self.assertEqual(snippet_detail, found.func)
-
-
-class EditSnippetTest(TestCase):
-    def test_should_resolve_snippet_edit(self):
-        found = resolve("/snippets/1/edit/")
-        self.assertEqual(snippet_edit, found.func)
+    def test_should_return_snippet_title(self):
+        request = RequestFactory().get("/")
+        request.user = self.user
+        response = top(request)
+        self.assertContains(response, self.snippet.title)
+    
+    def test_should_return_username(self):
+        request = RequestFactory().get("/")
+        request.user = self.user
+        response = top(request)
+        self.assertContains(response, self.user.username)
+    
