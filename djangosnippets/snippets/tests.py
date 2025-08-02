f3025b734e66119df8181b201e95ee2cd771d5cf
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2025-08-02 17:00:59 +0900

    Dev Detail page

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase, Client, RequestFactory
+from django.test import TestCase, RequestFactory
 
 from snippets.models import Snippet
 from snippets.views import top
@@ -33,3 +33,25 @@ class TopPageRenderSnippetsTest(TestCase):
         response = top(request)
         self.assertContains(response, self.user.username)
     
+
+class SnippetDetailTest(TestCase):
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            username="test_user",
+            email='test@example.com',
+            password="secret",
+        )
+        self.snippet = Snippet.objects.create(
+            title="タイトル",
+            code="コード",
+            description="解説",
+            created_by=self.user,
+        )
+    
+    def test_should_use_expected_template(self):
+        response = self.client.get("/snippets/%s/" % self.snippet.id)
+        self.assertTemplateUsed(response, "snippets/snippet_detail.html")
+    
+    def test_top_page_returns_200_and_expected_heading(self):
+        response = self.client.get("/snippets/%s/" % self.snippet.id)
+        self.assertContains(response, self.snippet.title, status_code=200)
